a3234e597eec67cf68290c3011046865b9f4e2b1
Nikita.Zmanovskiy <72348175+test212366@users.noreply.github.com>
2023-10-24 19:34:54 +0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Slider from 'react-slick';
+ 
+
+function App() {
+  return (
+		<div>
+			<Slider>
+				<div>
+					<p>321312</p>
+				</div>
+				<div>
+					<p>321312</p>
+				</div>
+				<div>
+					<p>321312</p>
+				</div>
+			</Slider>
+		</div>
+  );
+}
+
+export default App;
